0ff4c1f59b8d7aa13d3695e8ef91fb550a348513
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2015-09-01 22:44:48 +0900

    상세 페이지 url 추출

--- amazon_crawler/spiders/headphone.py
+++ amazon_crawler/spiders/headphone.py
@@ -16,18 +16,21 @@ class HeadphoneSpider(CrawlSpider):
         'http://www.amazon.com/s?_encoding=UTF8&ie=UTF8&rh=n%3A172541%2Cp_n_feature_browse-bin%3A509312&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0RHWRVH4HX5TMJCYT210&pf_rd_t=101&pf_rd_p=2187547522&pf_rd_i=172541']
     rules = (
         Rule(LinkExtractor(allow=('page='), restrict_css=('.pagnLink')),
-             process_links="filter_links"),
+             process_links="filter_links", callback='parse_catalog', follow=True),
         Rule(LinkExtractor(restrict_css=('.s-access-detail-page')),
              callback='parse_detail')
     )
 
-    def filter_links(self, links):
-        # for link in links:
-        #     self.logger.warning(link.url)
-        return links
+    # def filter_links(self, links):
+    # for link in links:
+    # self.logger.warning(link.url)
+    #     return links
 
     # def start_requests(self):
     #     pass
+    def parse_catalog(self, response):
+        self.logger.warning(response.url)
 
     def parse_detail(self, response):
-        self.logger.warning(response.url)
+        # self.logger.warning(response.url)
+        pass
